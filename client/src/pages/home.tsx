35bec4ab498dbb867cf727c15046d25ba0abd495
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-03-05 16:24:41 +0100

    feat(client): add title and PieChart to homepage

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,48 @@
 import { useList } from "@pankod/refine-core";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
+
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent,
+} from "components";
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart
+          title="Properties for sale"
+          value={684}
+          series={[75, 25]}
+          colors={["#475be8", "#e4e8ef"]}
+        />
+        <PieChart
+          title="Properties for rent"
+          value={550}
+          series={[60, 40]}
+          colors={["#47d2e8", "#e4asef"]}
+        />
+        <PieChart
+          title="Total Customers"
+          value={2684}
+          series={[15, 85]}
+          colors={["#412be8", "#f3e8ef"]}
+        />
+        <PieChart
+          title="Total Cities"
+          value={555}
+          series={[75, 25]}
+          colors={["#47f9e8", "#e4ebef"]}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default home;
