a82044f5fc2e8f4498d08ffeb40599d5691308cd
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-03-07 18:57:37 +0100

    feat(client): add select options to Form component

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -51,8 +51,77 @@ const Form = ({
             >
               Enter property name
             </FormHelperText>
-            <TextField />
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              {...register("title", { required: true })}
+            />
           </FormControl>
+
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px 0",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter property description
+            </FormHelperText>
+            <TextareaAutosize
+              minRows={5}
+              required
+              placeholder="Write description"
+              color="info"
+              style={{
+                width: "100%",
+                background: "transparent",
+                fontSize: "16px",
+                borderColor: "rgba(0,0,0,0.23",
+                borderRadius: 6,
+                padding: 10,
+                color: "#919191",
+              }}
+              {...register("description", { required: true })}
+            />
+          </FormControl>
+
+          <Stack direction="row" gap={4}>
+            <FormControl sx={{ flex: 1 }}>
+              <FormHelperText
+                sx={{
+                  fontWeight: 500,
+                  margin: "10px 0",
+                  fontSize: 16,
+                  color: "11142d",
+                }}
+              >
+                Select Property Type
+              </FormHelperText>
+              <Select
+                variant="outlined"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ "aria-label": "Without label" }}
+                defaultValue="apartment"
+                {...register("propertyType", { required: true })}
+              >
+                <MenuItem value="apartment">Apartment</MenuItem>
+                <MenuItem value="villa">Villa</MenuItem>
+                <MenuItem value="farmhouse">Farmhouse</MenuItem>
+                <MenuItem value="condos">Condos</MenuItem>
+                <MenuItem value="townhouse">Townhouse</MenuItem>
+                <MenuItem value="duplex">Duplex</MenuItem>
+                <MenuItem value="studio">Studio</MenuItem>
+                <MenuItem value="chalet">Chalet</MenuItem>
+              </Select>
+            </FormControl>
+          </Stack>
         </form>
       </Box>
     </Box>
