f012ea42a2b9188a9c6274038b8c75b72bcbe415
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-03-07 17:38:17 +0100

    feat(client): add form to Form component

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import {
+  Box,
+  Typography,
+  FormControl,
+  FormHelperText,
+  TextField,
+  TextareaAutosize,
+  Stack,
+  Select,
+  MenuItem,
+  Button,
+} from "@pankod/refine-mui";
 
-const Form = () => {
-  return <div>Form</div>;
+import { FormProps } from "interfaces/common";
+import CustomButton from "./CustomButton";
+
+const Form = ({
+  type,
+  register,
+  handleSubmit,
+  handleImageChange,
+  formLoading,
+  onFinishHandler,
+  propertyImage,
+}: FormProps) => {
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        {type} a Property
+      </Typography>
+
+      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#fcfcfc">
+        <form
+          style={{
+            marginTop: "20px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+          onSubmit={handleSubmit(onFinishHandler)}
+        >
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px 0",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter property name
+            </FormHelperText>
+            <TextField />
+          </FormControl>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default Form;
