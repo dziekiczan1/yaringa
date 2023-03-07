f378c31d488d1edf390713e46de4961e42141601
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-03-07 17:30:16 +0100

    feat(client): add Form component to create-property page

--- client/src/pages/create-property.tsx
+++ client/src/pages/create-property.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import { useGetIdentity } from "@pankod/refine-core/dist/hooks";
+import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
+import { useNavigate } from "@pankod/refine-react-router-v6";
+import Form from "components/common/Form";
+
 const CreateProperty = () => {
-  return <div>createProperty</div>;
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
+  const handleImageChange = () => {};
+  const onFinishHandler = () => {};
+
+  return (
+    <Form
+      type="Create"
+      register={register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      propertyImage={propertyImage}
+    />
+  );
 };
 
 export default CreateProperty;
