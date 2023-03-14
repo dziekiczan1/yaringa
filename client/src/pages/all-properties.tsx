38d1d96fb9aa812425278f7d40f230a5608442fc
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-03-14 16:37:03 +0100

    feat(client): add sorting fields and styles to allproperites page

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,6 +1,13 @@
 import { Add } from "@mui/icons-material";
 import { useTable } from "@pankod/refine-core";
-import { Box, Stack, Typography } from "@pankod/refine-mui";
+import {
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+} from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
 
 import { PropertyCard, CustomButton } from "components";
@@ -11,6 +18,14 @@ const AllProperties = () => {
 
   const {
     tableQueryResult: { data, isLoading, isError },
+    current,
+    setCurrent,
+    setPageSize,
+    pageCount,
+    sorter,
+    setSorter,
+    filters,
+    setFilters,
   } = useTable();
 
   const allProperties = data?.data ?? []; // if we dont have data its gonna be empty array
@@ -20,10 +35,58 @@ const AllProperties = () => {
 
   return (
     <Box>
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        <Stack direction="column" width="100%">
+          <Typography fontSize={25} fontWeight={700} color="#11142d">
+            {!allProperties.length
+              ? "There are no properties"
+              : "All Properties"}
+          </Typography>
+          <Box
+            mb={2}
+            mt={3}
+            display="flex"
+            width="84%"
+            justifyContent="space-between"
+            flexWrap="wrap"
+          >
+            <Box
+              display="flex"
+              gap={2}
+              flexWrap="wrap"
+              mb={{ xs: "20px", sm: 0 }}
+            >
+              <CustomButton
+                title={"Sort price"}
+                handleClick={() => {}}
+                backgroundColor="#475be8"
+                color="#fcfcfc"
+              />
+              <TextField
+                variant="outlined"
+                color="info"
+                placeholder="Search by title"
+                value=""
+                onChange={() => {}}
+              />
+              <Select
+                variant="outlined"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ "aria-label": "Without label" }}
+                defaultValue=""
+                value=""
+                onChange={() => {}}
+              >
+                <MenuItem value="">All</MenuItem>
+              </Select>
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
+
       <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Typography fontSize={25} fontWeight={700} color="#11142d">
-          All Properties
-        </Typography>
         <CustomButton
           title="Add Property"
           handleClick={() => navigate("/properties/create")}
