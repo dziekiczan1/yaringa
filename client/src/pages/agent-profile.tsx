f12fe06395d9819ec4142c7a28ee8f8d4598c28f
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-03-18 19:45:45 +0100

    feat(client): add agent=profile page

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -1,5 +1,30 @@
+import { useOne } from "@pankod/refine-core";
+import { useParams } from "react-router-dom";
+
+import { Profile } from "components";
+
 const AgentProfile = () => {
-  return <div>agentProfile</div>;
+  const { id } = useParams();
+
+  const { data, isLoading, isError } = useOne({
+    resource: "users",
+    id: id as string,
+  });
+
+  const myProfile = data?.data ?? [];
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
+  return (
+    <Profile
+      type="Agent"
+      name={myProfile.name}
+      email={myProfile.email}
+      avatar={myProfile.avatar}
+      properties={myProfile.allProperties}
+    />
+  );
 };
 
 export default AgentProfile;
